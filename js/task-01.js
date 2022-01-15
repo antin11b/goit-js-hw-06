95c103bd8ae4064e6e231302a0c37d8087240ece
Anton Bershak <anton.bershak@gmail.com>
2022-01-15 22:51:38 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -2,7 +2,7 @@ const allCategoriesEl = document.querySelectorAll('ul#categories li.item');
 
 console.log(`Number of categories: ${allCategoriesEl.length}`);
 
-const categoryEl = allCategoriesEl.forEach((category) => {
+allCategoriesEl.forEach((category) => {
     console.log(`\nCategory: ${category.firstElementChild.textContent}`);
     console.log(`Elements: ${category.querySelectorAll('li').length}`);
 });
