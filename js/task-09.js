1d1b11564ddeb032c369362d1c2520b84a87b14e
Anton Bershak <anton.bershak@gmail.com>
2022-01-16 19:31:05 +0200

    Add task 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const bodyEl = document.querySelector('body');
+const buttonEl = document.querySelector('button.change-color');
+const textEl = document.querySelector('span.color');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+const onClickButton = () => {
+  const currentColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = currentColor;
+  textEl.textContent = currentColor;
+};
+
+buttonEl.addEventListener('click', onClickButton);
