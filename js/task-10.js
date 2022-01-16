a120770f799a5b2db3545442bf58cafa158f675e
Anton Bershak <anton.bershak@gmail.com>
2022-01-16 23:38:37 +0200

    Add task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,32 @@
+const inputEl = document.querySelector('#controls input');
+const buttonCreatEl = document.querySelector('[data-create]');
+const buttonDestroyEl = document.querySelector('[data-destroy]');
+const divEl = document.querySelector('#boxes');
+
+function onClick() {
+  const amount = +inputEl.value;
+  creatBoxes(amount);
+ };
+  
+function creatBoxes(amount) {
+  const arrDivItemEl = [];
+  for (let i = 0; i < amount; i += 1) {
+  const divItemEl = document.createElement("div");
+  divItemEl.style.width = `${30+10*i}px`;
+  divItemEl.style.height = `${30 + 10 * i}px`;
+  divItemEl.style.backgroundColor = `${getRandomHexColor()}`;
+  arrDivItemEl.push(divItemEl);
+  divEl.append(...arrDivItemEl);
+  }
+};
+
+function destroyBoxes() {
+ divEl.innerHTML = '<div id="boxes"></div>';
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+buttonCreatEl.addEventListener('click', onClick);
+buttonDestroyEl.addEventListener('click', destroyBoxes);
