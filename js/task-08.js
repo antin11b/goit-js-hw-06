dde31bc599ce40ee4a814c94f041c3a40b8d509d
Anton Bershak <anton.bershak@gmail.com>
2022-01-16 18:42:59 +0200

    Add task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const formEl = document.querySelector('.login-form');
 
+const onFormElSubmit = (event) => {
+    event.preventDefault();
+
+    if (event.currentTarget.elements.email.value === '' && event.currentTarget.elements.password.value === '') {
+        alert('Введите значения!');
+    } else {
+
+        const email = event.currentTarget.elements.email.value;
+        const password = event.currentTarget.elements.password.value;
+
+        const formElData = {
+            email,
+            password,
+        };
+    
+        console.log(formElData);
+        formEl.reset();
+    }
+};
+
+formEl.addEventListener('submit', onFormElSubmit);
